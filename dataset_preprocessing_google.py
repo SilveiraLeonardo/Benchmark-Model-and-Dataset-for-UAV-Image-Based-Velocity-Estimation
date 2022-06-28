7cee8c994add70e7e00b93ebfca9ae014f719ff9
SilveiraLeonardo <47951669+SilveiraLeonardo@users.noreply.github.com>
2022-06-28 13:14:32 -0300

    Add files via upload

--- dataset_preprocessing_google.py
+++ dataset_preprocessing_google.py
@@ -5,7 +5,7 @@ import csv
 import pandas as pd
 
 # create CSV file and write header to it
-header = ['file1_name', 'file2_name', "delta_position_meters"]
+header = ['image1_path', 'image2_path', "delta_position_meters"]
 
 with open('data_pairs_google.csv', 'w', newline='') as f:
     writer = csv.writer(f)
@@ -24,7 +24,7 @@ for index, row in mydf.iterrows():
         distance_last_image = row["distance_last_image"]
         
         # write to CSV data
-        # ['file1_name', 'file2_name', "delta_position_meters"]
+        # ['image1_path', 'image2_path', "delta_position_meters"]
         text = [old_image_file, new_image_file, distance_last_image]
         with open('data_pairs_google.csv', 'a', newline='') as f:
             writer = csv.writer(f)
